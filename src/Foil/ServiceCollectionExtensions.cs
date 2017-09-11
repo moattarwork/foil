64d2cb0f6cbb639b3e95d82dc1f6223f97211607
Mohammad <moattarwork@msn.com>
2017-09-11 21:00:11 +0100

    Restructure the project and initial commit

--- src/Foil/ServiceCollectionExtensions.cs
+++ src/Foil/ServiceCollectionExtensions.cs
@@ -9,30 +9,58 @@ namespace Foil
 {
     public static class ServiceCollectionExtensions
     {
-        public static IServiceCollection AddTransientWithInterception<T, TImplementation>(this IServiceCollection services, Action<IInterceptBy> action) 
+        public static IServiceCollection AddTransientWithInterception<T, TImplementation>(
+            this IServiceCollection services, Action<IInterceptBy> action)
             where T : class
-            where TImplementation: class, T
+            where TImplementation : class, T
+        {
+            return AddWithInterception<T, TImplementation>(services, action, ServiceLifetime.Transient);
+        }
+
+        public static IServiceCollection AddScopedWithInterception<T, TImplementation>(
+            this IServiceCollection services, Action<IInterceptBy> action)
+            where T : class
+            where TImplementation : class, T
+        {
+            return AddWithInterception<T, TImplementation>(services, action, ServiceLifetime.Scoped);
+        }
+
+        public static IServiceCollection AddSingletonWithInterception<T, TImplementation>(
+            this IServiceCollection services, Action<IInterceptBy> action)
+            where T : class
+            where TImplementation : class, T
+        {
+            return AddWithInterception<T, TImplementation>(services, action, ServiceLifetime.Singleton);
+        }
+
+        private static IServiceCollection AddWithInterception<T, TImplementation>(this IServiceCollection services,
+            Action<IInterceptBy> action, ServiceLifetime lifetime)
+            where T : class
+            where TImplementation : class, T
         {
             if (services == null) throw new ArgumentNullException(nameof(services));
+            if (!Enum.IsDefined(typeof(ServiceLifetime), lifetime))
+                throw new ArgumentOutOfRangeException(nameof(lifetime),
+                    "Value should be defined in the ServiceLifetime enum.");
 
             var interceptionOptions = new InterceptionOptions();
             action?.Invoke(interceptionOptions);
 
             interceptionOptions.Interceptors.ForEach(services.TryAddTransient);
-            services.TryAddTransient<TImplementation>();
+
+            services.TryAdd(ServiceDescriptor.Describe(typeof(TImplementation), typeof(TImplementation), lifetime));
 
             services.AddTransient(sp =>
             {
-                var interceptorInstances = interceptionOptions.Interceptors.Select(sp.GetRequiredService).Cast<IInterceptor>().ToArray();
+                var interceptorInstances = interceptionOptions.Interceptors.Select(sp.GetRequiredService)
+                    .Cast<IInterceptor>().ToArray();
                 var implementation = sp.GetRequiredService<TImplementation>();
-                
+
                 var proxyFactory = new ProxyGenerator();
                 return proxyFactory.CreateInterfaceProxyWithTarget<T>(implementation, interceptorInstances);
             });
 
             return services;
         }
-        
-        
     }
 }
