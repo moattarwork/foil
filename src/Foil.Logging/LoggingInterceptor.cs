58405fd14c7316dbf813dfc13e9499f0ab1d4495
moattarwork <moattarwork@msn.com>
2018-11-16 13:15:40 +0000

    Included async interception and fix the bug with multiple interception

---
+++ src/Foil.Logging/LoggingInterceptor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Castle.DynamicProxy;
+using Microsoft.Extensions.Logging;
+
+namespace Foil.Logging
+{
+    public sealed class LoggingInterceptor : IInterceptor
+    {
+        private readonly ILogger<LoggingInterceptor> _logger;
+
+        public LoggingInterceptor(ILogger<LoggingInterceptor> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+        
+        public void Intercept(IInvocation invocation)
+        {
+            _logger.LogInformation($"Executing Method: {invocation.Method.Name}");
+
+            LogInvocationInfo(invocation);
+            
+            invocation.Proceed();
+            
+            _logger.LogInformation($"Executed Method: {invocation.Method.Name}");
+        }
+
+        private void LogInvocationInfo(IInvocation invocation)
+        {
+            if (_logger.IsEnabled(LogLevel.Trace))
+                _logger.LogTrace(invocation.FormatArguments());
+        }
+    }
+}
